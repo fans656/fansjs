8340ce3102fe0800beda872e9fda4c4843436aaf
fans656 <fans656@yahoo.com>
2024-11-09 11:00:24 +0800

    build library

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+import _ from 'lodash';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+import packageJson from './package.json';
+
+export default defineConfig(({mode}) => {
+  let config = {
+    define: {
+      __VERSION__: JSON.stringify(packageJson.version),
+    },
+    build: {
+      outDir: mode === 'app' ? 'dist.app' : 'dist',
+    },
+    plugins: [
+      react(),
+    ],
+  };
+  if (mode !== 'app') {
+    config = _.merge(config, {
+      build: {
+        lib: {
+          entry: path.resolve(__dirname, 'src/index.js'),
+          name: 'fansjs',
+          fileName: (format) => `fansjs.${format}.js`
+        },
+      },
+    });
+  }
+  return config;
 })
