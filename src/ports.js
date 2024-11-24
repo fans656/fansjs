456bc469bd06952eb2a5da0fcd8ae02a5c4a43ab
fans656 <fans656@yahoo.com>
2024-11-24 20:46:35 +0800

    update ports

--- src/ports.js
+++ src/ports.js
@@ -1,9 +1,12 @@
 export const ports = {
-  'fme': 6561,
-  'auth': 6562,
-  'quantix': 6563,
-  'stome': 6565,
-  'stome_desktop': 6660,
+  'auth':           6560,
+  'fme':            6561,
+  'quantix':        6562,
+  'doy':            6563,
 
-  'fansjs_dev': 16501,
+  'stome':          6565,
+
+  'stome_desktop':  6660,
+
+  'fansjs_dev':     16501,
 };
